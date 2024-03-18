711a4b1a295c696498729ed66f196f9c6070f781
Alece-98 <Felbec0ppens@hotmail.com>
2024-03-18 15:04:56 +0100

    Update Room.php

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -7,76 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 use App\Enums\RoomType;
 use App\Enums\RoomView;
 use App\Models\ReservationTask;
-use Illuminate\Database\Eloquent\Relations;
 
 
 class Room extends Model
 {
     use HasFactory;
 
-
     //Table name for database
     protected $table = 'rooms';
-
-    protected $fillable = ['room_id', 'number', 'floor', 'view', 'type', 'handicap_accessible', 'baby_bed', 'price_per_night'];
-
-    private int $roomNumber = 0;
-    private int $singleBeds = 0;
-    private int $twinBeds = 0;
-    private int $floor = 0; #Moet dit een int zijn?
-    private RoomView $roomView = RoomView::Standard;
-    private RoomType $roomType = RoomType::Standard;
-    private bool $hasHandicapFacility = false;
-    private bool $hasBabyBedOption = false;
-    private array $cleaningTasks = [];
-    private array $maintenanceTasks = [];
-    private array $bookingTasks = [];
-    private array $reservations = [];
-    private int $price = 0;
-    private string $uuid;
-
 
     public function __construct(){
         #UUID komt hier
     }
 
-
     // Room number
     public function getRoomNumber(): int {
-        return $this->attributes['number'];
+        return $this->attributes['room_number'];
     }
 
     public function setRoomNumber(int $roomNumber): void {
        //$this->roomNumber = $roomNumber;
-
-       $this->attributes['number'] = $roomNumber;
-    }
-
-    public function reservation() : HasMany {
-        $this->hasMany(ReservationTask::class);
-    }
-
-    // Single beds
-    /*public function getSingleBeds(): int {
-
        $this->attributes['room_number'] = $roomNumber;
-
-        // Room number
-    public function getRoomNumber(): int {
-        return $this->roomNumber;
-    }
-
-    public function setRoomNumber(int $roomNumber): void {
-        $this->roomNumber = $roomNumber;
-
     }
 
     // Single beds
     public function getSingleBeds(): int {
-
-        return $this->singleBeds;
-
-
     }
 
     public function setSingleBeds(int $singleBeds): void {
@@ -91,53 +46,35 @@ class Room extends Model
     public function setTwinBeds(int $twinBeds): void {
         $this->twinBeds = $twinBeds;
     }
-    */
-
-    public function getBeds(){
-        return $this->attributes['beds'];
-    }
-
-    public function setBeds(string $beds){
-        $this->attributes['beds'] = $beds;
-    }
 
     // Floor
     public function getFloor(): int {
-
         return $this->attributes['floor'];
     }
 
     public function setFloor(int $floor): void {
         $this->attributes['floor'] = $floor;
-
-        return $this->floor;
-    }
-
-    public function setFloor(int $floor): void {
-        $this->floor = $floor;
-
     }
 
     // Room view
     public function getRoomView(): RoomView {
-        return RoomView::tryFrom($this->attributes['view']);
+        return $this->roomView;
     }
 
     public function setRoomView(string $roomView): void {
-        $this->attributes['view'] = RoomView::tryFrom($roomView);
+        $this->roomView = RoomView::tryFrom($roomView);
     }
 
     // Room type
     public function getRoomType(): RoomType {
-        return RoomType::tryFrom($this->attributes['type']);
+        return $this->roomType;
     }
 
     public function setRoomType(string $roomType): void {
-        $this->attributes['type'] = RoomType::tryFrom($roomType);
+        $this->roomType = RoomType::tryFrom($roomType);
     }
 
     // Handicap facility
-
     public function isHandicapAccessible(): bool {
         return $this->attributes['handicap_accessible'];
     }
@@ -145,67 +82,46 @@ class Room extends Model
     public function setHandicapAccessible(bool $isHandicapAccessible): void {
         $this->attributes['handicap_accessible'] = $isHandicapAccessible;
     }
-    public function hasHandicapFacility(): bool {
-        return $this->hasHandicapFacility;
-    }
-
-    public function setHasHandicapFacility(bool $hasHandicapFacility): void {
-        $this->hasHandicapFacility = $hasHandicapFacility;
-
-    }
 
     // Baby bed option
-
-
     public function hasBabyBedOption(): bool {
-
-
         return $this->attributes['baby_bed'];
     }
 
-    public function setHasBabyBed(bool $hasBabyBedOption): void {
-        $this->attributes['baby_bed'] = $hasBabyBedOption;
-
-        return $this->hasBabyBedOption;
-    }
-
     public function setHasBabyBedOption(bool $hasBabyBedOption): void {
-        $this->hasBabyBedOption = $hasBabyBedOption;
-
+        $this->attributes['baby_bed'] = $hasBabyBedOption;
     }
 
     // Cleaning tasks
     public function getCleaningTasks(): array {
+        return $this->cleaningTasks;
     }
 
     public function setCleaningTasks(array $cleaningTasks): void {
+        $this->cleaningTasks = $cleaningTasks;
     }
 
     // Maintenance tasks
     public function getMaintenanceTasks(): array {
+        return $this->maintenanceTasks;
     }
 
     public function setMaintenanceTasks(array $maintenanceTasks): void {
+        $this->maintenanceTasks = $maintenanceTasks;
     }
 
     // Booking tasks
     public function getBookingTasks(): array {
+        return $this->bookingTasks;
     }
 
     public function setBookingTasks(array $bookingTasks): void {
+        $this->bookingTasks = $bookingTasks;
     }
 
     // Reservations
-
-    public function getReservations(): HasMany {
-        return $this->hasMany(ReservationTask::class);
-    }
     public function getReservations(): array {
-
         return [];
-
-        return $this->reservations;
-
     }
 
     public function setReservations(array $reservations): void {
@@ -213,8 +129,6 @@ class Room extends Model
     }
 
     public function addReservation(ReservationTask $reservation): void{
-
-
     }
 
     // Price
@@ -224,18 +138,7 @@ class Room extends Model
 
     public function setPricePerNight(int $price): void {
         $this->attributes['price_per_night'] = $price;
-
-        array_push($this->reservations, $reservation);
-    }
-
-    // Price
-    public function getPrice(): int {
-        return $this->price;
-    }
-
-    public function setPrice(int $price): void {
-        $this->price = $price;
-
     }
 }
+
 
