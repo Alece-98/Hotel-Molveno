0c9160f152d275ee8cd347ef161bcde2b5dc38e8
Alece-98 <Felbec0ppens@hotmail.com>
2024-04-08 14:04:57 +0200

    into dev
    g

--- app/Http/Controllers/extraGuestController.php
+++ app/Http/Controllers/extraGuestController.php
@@ -15,6 +15,7 @@ class extraGuestController extends Controller
     public function store(Request $request){
 
         $request->validate([
+<<<<<<< Updated upstream
             'extraGuestName' => 'required|max:32',
             'extraGuestLastName'=> 'required|max:32',
             'extraGuestPhone' => 'required|max:10',
@@ -24,6 +25,17 @@ class extraGuestController extends Controller
             'extraGuestCity' => 'required|max:255',
             'extraGuestZipcode' => 'required|max:6',
             'extraGuestCountry' => 'required|max:32'
+=======
+            // 'extraGuestName' => 'required|max:32',
+            // 'extraGuestLastName'=> 'required|max:32',
+            // 'extraGuestPhone' => 'required|max:10',
+            // 'extraGuestEmail' => 'required|email',
+            // 'extraGuestAdress' => 'required|max:255',
+            // 'extraGuestHouseNumber' =>'required|max:10',
+            // 'extraGuestCity' => 'required|max:255',
+            // 'extraGuestZipcode' => 'required|max:6',
+            // 'extraGuestCountry' => 'required|max:32'
+>>>>>>> Stashed changes
 
         ]);
         $extraGuest = new extraGuest();
@@ -31,7 +43,11 @@ class extraGuestController extends Controller
         $extraGuest->extraGuestLastName = $request->input('extraGuestLastName');
         $extraGuest->extraGuestPhone = $request->input('extraGuestPhone');
         $extraGuest->extraGuestEmail = $request->input('extraGuestEmail');
+<<<<<<< Updated upstream
         $extraGuest->extraguestAdress = $request->input('extraGuestAdress');
+=======
+        // $extraGuest->extraguestAdress = $request->input('extraGuestAdress');
+>>>>>>> Stashed changes
         $extraGuest->extraGuestHouseNumber = $request->input('extraGuestHouseNumber');
         $extraGuest->extraGuestCity = $request->input('extraGuestCity');
         $extraGuest->extraGuestZipcode = $request->input('extraGuestZipcode');
