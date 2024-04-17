886aaab1de06a81f13f8c6dc2c8716e65a55f669
ddsnijders <djowie.snijders@gmail.com>
2024-04-17 15:12:21 +0200

    Added message when no rooms are available

--- resources/views/selectReservationRoom.blade.php
+++ resources/views/selectReservationRoom.blade.php
@@ -2,6 +2,12 @@
 <x-MasterLayout>
 
 <body>
+    @csrf
+    @if ($rooms->count() == 0)
+        <div class="h-12 m-4 blocked flex" id="emptyerror" name="emptyerror">
+            <h1>No rooms are available that satisfy the given conditions</h1>
+        </div>
+    @else
     <div class="m-8">
         <div class="grid grid-cols-6 divide-x h-12 m-4">
             <div>Room Number</div>
@@ -26,6 +32,7 @@
             </div>
         </button>
     @endforeach 
+    @endif
     </form>
 </body>
 </x-MasterLayout>
