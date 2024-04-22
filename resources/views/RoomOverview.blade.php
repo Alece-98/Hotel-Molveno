9fe861188381d188130a768eef3241f8f887953e
sabrina <sabrinahermans@live.nl>
2024-04-22 10:29:02 +0200

    price is working

--- resources/views/RoomOverview.blade.php
+++ resources/views/RoomOverview.blade.php
@@ -3,20 +3,23 @@
     <div class="overviewContainer flex flexVertical">
 
         <div class="listHeader">
-            <p class="fixedWidth">Room Number</p>
-            <p class="fixedWidth">Room Class</p>
-            <p class="fixedWidth">View</p>
-            <p class="fixedWidth">Guest Capacity</p>
-            <p class="fixedWidth">Beds</p>
+            <p class="fixedWidth15">Room Number</p>
+            <p class="fixedWidth15">Room Class</p>
+            <p class="fixedWidth15">View</p>
+            <p class="fixedWidth15">Guest Capacity</p>
+            <p class="fixedWidth20">Beds</p>
+            <p class="fixedWidth15">Price per Night</p>
+
         </div>
 
         @foreach ($rooms as $room)
             <div class="listRow available">
-                <p class="fixedWidth">{{$room->number}}</p>
-                <p class="fixedWidth">{{$room->type}}</p>
-                <p class="fixedWidth">{{$room->view}}</p>
-                <p class="fixedWidth">{{$room->capacity}}</p>
-                <p class="fixedWidth">{{$room->bed_description}}</p>
+                <p class="fixedWidth15">{{$room->number}}</p>
+                <p class="fixedWidth15">{{$room->type}}</p>
+                <p class="fixedWidth15">{{$room->view}}</p>
+                <p class="fixedWidth15">{{$room->capacity}}</p>
+                <p class="fixedWidth20">{{$room->bed_description}}</p>
+                <p class="fixedWidth15">&euro; {{$room->price_per_night}} p/n</p>
             </div>
         @endforeach
 
